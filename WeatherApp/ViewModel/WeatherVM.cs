09292fe2c6ea82343b9f21c9d16d33237d1bf6ae
Anh Bui <bui_anh@digital-sol.co.jp>
2023-01-18 15:03:16 +0900

    first commit

---
+++ WeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+using WeatherApp.ViewModel.Commands;
+using WeatherApp.ViewModel.Helpers;
+
+namespace WeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set
+            {
+                query = value;
+                OnPropertyChanged("Query");
+            }
+        }
+
+        private CurrentCondition currentCondition;
+
+        public CurrentCondition CurrentCondition
+        {
+            get { return currentCondition; }
+            set
+            {
+                currentCondition = value;
+                OnPropertyChanged("CurrentCondition");
+            }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+                GetCurrentCondition();
+            }
+        }
+
+        public SearchCommand SearchCommand { get; set; }
+        public ObservableCollection<City> Cities { get; set; }
+
+        public WeatherVM()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+            {
+                SelectedCity = new City()
+                {
+                    LocalizedName = "New York"
+                };
+                CurrentCondition = new CurrentCondition()
+                {
+                    WeatherText = "Partly cloudly",
+                    Temperature = new Temperature()
+                    {
+                        Metric = new Units()
+                        {
+                            Value = "21"
+                        }
+                    }
+                };
+            }
+
+            SearchCommand = new SearchCommand(this);
+            Cities = new ObservableCollection<City>();
+        }
+
+        public async void MakeQuery()
+        {
+            var cities = await AccuWeatherHelper.GetCities(Query);
+
+            Cities.Clear();
+            foreach (var city in cities)
+            {
+                Cities.Add(city);
+            }
+        }
+
+        public async void GetCurrentCondition()
+        {
+            Query = string.Empty;
+            Cities.Clear();
+            CurrentCondition = await AccuWeatherHelper.GetCurrentCondition(SelectedCity.Key);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
